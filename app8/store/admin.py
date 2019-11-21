2f41cc71282bff18c3060c8c5a25239eb6f7f462
wafistos1 <mameri.wafi@gmail.com>
2019-11-21 20:38:50 +0100

    edit template recherche and aliment

--- app8/store/admin.py
+++ app8/store/admin.py
@@ -8,4 +8,6 @@ class FavoriteAdmin(admin.ModelAdmin):
     search_fields = ['pk', 'user__username']  
     """
     * * important :search_fields = ['foreign_key__related_fieldname']  exemple: search_fields = ['user__email']
+    
     """
+    
